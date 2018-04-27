90c7ed4204d8096585c330712af166d2e765818c
Matt Kehrls <matthew.kehrls@gmail.com>
2018-04-27 12:48:27 -0400

    fixing the github warnings

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs'); //Sets up the view engine installed by NPM 'hbs'
+app.use(express.static(__dirname + '/public')); //Must include file type in URL to hit the page
 
-app.use((req, res, next) => {
+hbs.registerHelper('getCurrentYear', () => {//Helpers are for running functions to recieve data back and use it in a template, by calling the name of the helper.
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {//Pass data to a helper by adding a space after you call the name.(See Footer.hbs)
+    return text.toUpperCase();
+})
+
+app.use((req, res, next) => { //app.use is how you register "middleware", next tells the middleware when its finished,handlers(app.get()) wont fire if not called! logs server log into a file when the app gets "used"
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
     console.log(log);
@@ -24,12 +33,10 @@ app.use((req, res, next) => {
 //     res.render('maintance.hbs');
 // });
 
-app.use(express.static(__dirname + '/public')); //Must include file type in URL to hit the page
-
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'The Home Page',
-        pageBody: 'The body of the page',
+        welcomeMessage: 'The body of the page',
         currentYear: new Date().getFullYear()
     });
     // res.send('<h1>Hello Express!</h1>');
